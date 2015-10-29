95d457fe60b713f66761147191e665d4d217774c
Chad Killingsworth <chad.killingsworth@banno.com>
2015-10-29 17:46:51 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,63 @@
+# bower-sinopia-resolver
+
+A custom Bower resolver supporting scoped npm packages on a sinopia server to be installed
+
+## Installation
+
+    npm install -g bower-sinopia-resolver
+
+## Client Configuration
+
+### Global .bowerrc
+
+Create or edit a `.bowerrc` file in your home director (~/.bowerrc).
+
+Add bower-sinopia-resolver to a resolvers section
+
+```Javascript
+  "resolvers": [
+    "bower-sinopia-resolver"
+  ]
+```
+
+Add a bower-sinopia-resolver configuration
+
+```Javascript
+  "bower-sinopia-resolver": {
+    "scopes": {
+      
+      // scope would be @myco
+      "myco": { 
+        //uri of your sinopia server 
+        "server": "https://sinopia.myco.com",
+        
+        //optional - allows strict ssl support for self-signed or private certificates
+        "cafile": "/path/to/certificate.pem"
+      }
+    }
+  }
+```
+
+## Usage
+
+Once configured, your bower.json files may reference packages using [npm scope syntax](https://docs.npmjs.com/misc/scope):
+
+```JavaScript
+  "dependencies": {
+    "@myco/package-name": "1.0.0"
+  }
+```
+
+Even though referenced with the scope, pacakages will install without the @myco scope prefixed name.
+
+## Notes
+
+Authentication is not currently supported - packages are downloaded anonymously.
+
+## License
+
+Copyright 2015 [Jack Henry & Associates Inc](https://www.jackhenry.com/).
+
+Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with the License. You may obtain a copy of the License at [http://www.apache.org/licenses/LICENSE-2.0](http://www.apache.org/licenses/LICENSE-2.0).
+
+Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the specific language governing permissions and limitations under the License.
---
+++ index.js
@@ -0,0 +1,149 @@
+
+var request = require('request');
+var fs = require('fs');
+var Q = require('q');
+var tmp = require('tmp');
+var download = require('./download');
+var extract = require('./extract');
+var object = require('mout/object');
+
+tmp.setGracefulCleanup();
+
+/**
+ * Factory function for resolver
+ * It is called only one time by Bower, to instantiate resolver.
+ * You can instantiate here any caches or create helper functions.
+ */
+module.exports = function resolver (bower) {
+  var config = bower.config.bowerSinopiaResolver || {};
+  config.scopes = config.scopes || {};
+
+  var keysToDelete = [];
+  for (var key in config.scopes) {
+    if (config.scopes[key].cafile) {
+      config.scopes[key].ca = fs.readFileSync(config.scopes[key].cafile);
+    }
+
+    if (key !== key.toLowerCase()) {
+      config.scopes[key.toLowerCase()] = config.scopes[key];
+      keysToDelete.push(key);
+    }
+  }
+  for(var i = 0; i < keysToDelete.length; i++) {
+    delete config.scopes[keysToDelete[i]];
+  }
+
+  var scopeParts = /^@([-a-z]+)\//i;
+  var packageNameParts = /^@[-a-z]+\/([^=]+)/i;
+
+  function reqOptsForScope(scope) {
+    var reqDefaults = {};
+    if (config.scopes[scope].ca) {
+      reqDefaults.ca = config.scopes[scope].ca;
+    }
+    return reqDefaults;
+  }
+
+  function scopeForSource(source) {
+    var scopeMatches = scopeParts.exec(source);
+    if(!scopeMatches) {
+      return null;
+    }
+
+    return scopeMatches[1].toLowerCase();
+  }
+
+  function packageNameForSource(source) {
+    var packageMatches = packageNameParts.exec(source);
+    if(!packageMatches) {
+      return null;
+    }
+
+    return packageMatches[1].toLowerCase();
+  }
+
+  // Resolver factory returns an instance of resolver
+  return {
+    // Match method tells whether resolver supports given source
+    // It can return either boolean or promise of boolean
+    match: function (source) {
+      var scope = scopeForSource(source);
+      if(!scope) {
+        return false;
+      }
+
+      return scope in config.scopes;
+    },
+
+    // Allows to list available versions of given source.
+    // Bower chooses matching release and passes it to "fetch"
+    releases: function (source) {
+      var scope = scopeForSource(source);
+      var packageName = packageNameForSource(source);
+
+      var request_ = request.defaults(reqOptsForScope(scope));
+      var deferred = Q.defer();
+
+      var packageInfoUri = config.scopes[scope].server + '/@' +
+          scope + '%2f' + packageName + '/';
+
+      request_(packageInfoUri, function(error, response, body) {
+        if (error) {
+          deferred.reject(new Error('Request to ' + packageInfoUri + ' failed: ' + error.message));
+          return;
+        }
+        if (response.statusCode !== 200) {
+          deferred.reject(new Error('Request to ' + packageInfoUri + ' returned ' + response.statusCode + ' status code.'));
+          return;
+        }
+
+        var respObject;
+
+        try {
+          respObject = JSON.parse(body);
+        } catch (e) {
+          deferred.reject(e);
+        }
+
+        var versions = [];
+        for(var version in respObject.versions) {
+          versions.push({
+            target: respObject.versions[version].dist.tarball,
+            version: version
+          });
+        }
+
+        deferred.resolve(versions);
+      });
+
+      return deferred.promise;
+    },
+
+    fetch: function (endpoint, cached) {
+      // If cached package is semver, reuse it
+      if (cached.version) {
+        return;
+      }
+
+      var scope = scopeForSource(endpoint.source);
+
+      var request_ = request.defaults(reqOptsForScope(scope));
+
+      var downloadPath = tmp.dirSync();
+
+      return download(endpoint.target, downloadPath.name,
+          object.mixIn({ request: request_}, bower.config)).then(function (archivePatch) {
+        var extractPath = tmp.dirSync();
+
+        return extract(archivePatch, extractPath.name).then(function () {
+          downloadPath.removeCallback();
+
+          return {
+            tempPath: extractPath.name,
+            removeIgnores: true
+          };
+        });
+      });
+    }
+  }
+};
